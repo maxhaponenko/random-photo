bd0e1c19ab6a1f38eef06f6ee7ff9dd001246951
Max Haponenko <maksym@transferroom.com>
2022-02-19 23:42:28 +0200

    Redux actions added

--- src/store/image-selector.slice.js
+++ src/store/image-selector.slice.js
@@ -2,25 +2,35 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { ImageService } from 'api/image.service';
 
 const initialState = {
+  isSelectProcessStarted: false,
   isLoading: false,
   candidate: null,
   items: []
 };
 
 export const fetchRandomImage = createAsyncThunk(
-  'images/loadRandom',
+  'image-selector/loadRandom',
   ImageService.emulateLoadImage
 )
 
 export const counterSlice = createSlice({
-  name: 'images',
+  name: 'image-selector',
   initialState,
   reducers: {
     addImage: (state, action) => {
       state.items.push(action.payload)
     },
+    skipImage: (state) => {
+      state.candidate = null
+    },
     deleteImage: (state, action) => {
-      state.items = state.items.filter(item => item !== action.payload)
+      state.items = state.items.filter(item => item.id !== action.payload)
+    },
+    disposeState: (state) => {
+      state.isSelectProcessStarted = false
+      state.isLoading = false
+      state.candidate = null
+      localStorage.setItem('wasDone', JSON.stringify(state))
     }
   },
   
@@ -28,18 +38,19 @@ export const counterSlice = createSlice({
     builder
       .addCase(fetchRandomImage.pending, (state) => {
         state.isLoading = true;
+        state.isSelectProcessStarted = true;
         console.log('fulfilled')
       })
       .addCase(fetchRandomImage.fulfilled, (state, action) => {
         state.isLoading = false
-        state.candidate = action.payload.urls.regular
+        state.candidate = action.payload
       })
   },
 });
 
-export const { addImage, deleteImage } = counterSlice.actions;
+export const { addImage, skipImage, deleteImage, disposeState } = counterSlice.actions;
 
-export const selectImages = (state) => state.images.items;
-export const selectIsLoading = (state) => state.images.isLoading;
+export const selectImages = (state) => state.imageSelector.items;
+export const selectIsLoading = (state) => state.imageSelector.isLoading;
 
 export default counterSlice.reducer;
